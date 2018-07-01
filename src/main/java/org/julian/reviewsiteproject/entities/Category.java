65fd302b19d39f19c7509f3f21fe5e190977a2e6
julian-perge <julianperge@gmail.com>
2018-06-30 20:47:33 -0400

    Added entities folder for all entities

---
+++ src/main/java/org/julian/reviewsiteproject/entities/Category.java
@@ -0,0 +1,49 @@
+package org.julian.reviewsiteproject.entities;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Category
+{
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String name;
+
+	@OneToMany(mappedBy = "category")
+	private Collection<Review> reviews;
+
+	protected Category() {}
+
+	public Category(String name)
+	{
+		this.name = name;
+	}
+	
+	
+	public Collection<Review> getReviews() {
+		return reviews;
+	}
+
+	@Override
+	public String toString()
+	{
+		return name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public Long getId()
+	{
+		return id;
+	}
+}
---
+++ src/main/java/org/julian/reviewsiteproject/entities/Review.java
@@ -0,0 +1,78 @@
+package org.julian.reviewsiteproject.entities;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Review
+{
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String title;
+	private String imgUrl;
+
+	@ManyToMany
+	private Collection<Tag> tags;
+
+	@ManyToOne
+	private Category category;
+
+	@Lob
+	private String content;
+
+	protected Review(){}
+
+	public Review(String title, String imgUrl, String content, Category category , Tag ...tags)
+	{
+		this.title = title;
+		this.imgUrl = imgUrl;
+		this.content = content;
+		this.category = category;
+		this.tags = Arrays.asList(tags);
+	}
+	
+	@Override
+	public String toString()
+	{
+		return title + " - " + category;
+	}
+
+	public Long getId()
+	{
+		return id;
+	}
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public String getImgUrl()
+	{
+		return imgUrl;
+	}
+
+	public Category getCategory()
+	{
+		return category;
+	}
+
+	public String getContent()
+	{
+		return content;
+	}
+
+	public Collection<Tag> getTags()
+	{
+		return tags;
+	}
+}
