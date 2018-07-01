f07103469ca29fde64a79ffc94bcee116d6dc0d6
julian-perge <julianperge@gmail.com>
2018-06-30 20:46:50 -0400

    Added controller folder for list of current controllers

---
+++ src/main/java/org/julian/reviewsiteproject/controller/ReviewsController.java
@@ -0,0 +1,32 @@
+package org.julian.reviewsiteproject.controller;
+
+import org.julian.reviewsiteproject.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReviewsController
+{
+	@Autowired
+	private ReviewRepository reviewRepo;
+
+	// localhost:8080/reviews
+	// thuglyfe
+	
+	@RequestMapping("/reviews")
+	public String getReviews(Model model)
+	{
+		model.addAttribute("reviews", reviewRepo.findAll());
+		return "reviews";
+	}
+	
+	@RequestMapping("/reviews/{title}")
+	public String getReview(@PathVariable(name="title")String title, Long id, Model model)
+	{
+		model.addAttribute("review", reviewRepo.findByTitle(title));
+		return "review";
+	}
+}
---
+++ src/main/java/org/julian/reviewsiteproject/controller/TagController.java
@@ -0,0 +1,29 @@
+package org.julian.reviewsiteproject.controller;
+
+import org.julian.reviewsiteproject.repository.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TagController {
+	
+	@Autowired
+	private TagRepository tagRepo;
+	
+	@RequestMapping("/reviews/tags")
+	public String getTags(Model model)
+	{
+		model.addAttribute("tags", tagRepo.findAll());
+		return "tags";
+	}
+	
+	@RequestMapping("/reviews/tags/{name}")
+	public String getListOfMoviesFromTag(@PathVariable(name="name")String name,  Model model)
+	{
+		model.addAttribute("tag", tagRepo.findByName(name));
+		return "tag";
+	}
+}
