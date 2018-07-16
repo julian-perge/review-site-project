f2b0b9d4c11bdf5b85a07bb6575021e3d44f0f81
julian-perge <dpstank@gmail.com>
2018-07-16 01:42:07 -0400

    bin files don't matter lol

--- bin/main/static/src/js/app.js
+++
@@ -1,7 +0,0 @@
-let reviewItem = document.querySelectorAll('review-items > li');
-
-let reviewText = reviewItem.textContent;
-
-console.log(reviewText);
-
-const tags = document.querySelector('.tags');
