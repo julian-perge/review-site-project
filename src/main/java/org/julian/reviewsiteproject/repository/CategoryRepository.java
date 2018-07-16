4b76b506f8f80f0ee43d350020586f6050167cf0
julian-perge <dpstank@gmail.com>
2018-07-16 01:45:46 -0400

    Removed @Repository annotation

--- src/main/java/org/julian/reviewsiteproject/repository/CategoryRepository.java
+++ src/main/java/org/julian/reviewsiteproject/repository/CategoryRepository.java
@@ -4,7 +4,6 @@ import org.julian.reviewsiteproject.entities.Category;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CategoryRepository extends CrudRepository<Category, Long>
 {
 	public Category findByName(String name);
