7af410be0ada5f2e555f3fe1258309a8e3b591d8
julian-perge <julianperge@gmail.com>
2018-07-16 09:54:15 -0400

    merge

--- src/main/resources/static/src/js/app.js
+++ src/main/resources/static/src/js/app.js
@@ -1,4 +1,13 @@
 let reviewItem = document.querySelectorAll('review-items > li');
+const tags = document.querySelector('.tags-items');
+tags.addEventListener('click', removeTag);
+
+function removeTag(event) {
+    if (event.target.classList.contains('delete')) {
+        const deleteButton = event.target;
+        
+    }
+}
 
 let reviewText = reviewItem.textContent;
 
